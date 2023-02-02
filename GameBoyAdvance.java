735beefad1d9b04f3e59ef72a052cd4ab0d45870
usov_boris <busov@ukr.net>
2023-02-02 23:48:46 +0200

    new GameAdvance file

---
+++ GameBoyAdvance.java
@@ -0,0 +1,10 @@
+class GameBoyAdvance {
+    String cartrigeType; //type of cassete
+    int screenWidth; //shirina ekrana
+    void startGame () {
+
+    }
+    void stopGame () {
+        
+    }
+}
