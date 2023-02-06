fb91f612dc9855d3c287cf588ec02325fcf7a9ef
usov_boris <busov@ukr.net>
2023-02-06 22:56:19 +0200

    new float and final var in HelloWorld file

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,12 @@
 public class HelloWorld {
 
 public static void main(String[] args) {
-int myScore=10;
+float myScore=10;
+    myScore=myScore/4;
+final String MY_TOWN="Lviv";    
        System.out.println("Hello World!");
-       System.out.println(" Spasibo Mama!" +  myScore);
+       System.out.println("Spasibo Mama!" +  myScore);
+       System.out.println(MY_TOWN +  myScore);
      }
 }
 
