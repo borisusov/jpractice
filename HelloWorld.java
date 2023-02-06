9b08963759f909970626b5201b61856aefa2cbc5
usov_boris <busov@ukr.net>
2023-02-06 22:10:25 +0200

    new int var in HelloWorld file

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,9 @@
 public class HelloWorld {
 
 public static void main(String[] args) {
-
+int myScore=10;
        System.out.println("Hello World!");
+       System.out.println(" Spasibo Mama!" +  myScore);
      }
 }
 
